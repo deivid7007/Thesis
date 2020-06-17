dadaa992989798c398950e308c9829f9a1d5206c
Deivid Mladenov <dmladenov@zingasoft.com>
2020-06-17 17:56:59 +0300

    Version 1.2

---
+++ IntroToProgrammingCSharpTasks/NumbersInDifferentNumeralSystems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumbersInDifferentNumeralSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 555;
+
+            string binary = Convert.ToString(number, 2);
+            string ocataldecimal = Convert.ToString(number, 8);
+            string hexadecimal = Convert.ToString(number, 16);
+
+            Console.WriteLine($"The number 555 in binary is {binary}");
+            Console.WriteLine($"The number 555 in ocataldecimal is {ocataldecimal}");
+            Console.WriteLine($"The number 555 in hexadecimal is {hexadecimal}");
+        }
+    }
+}
