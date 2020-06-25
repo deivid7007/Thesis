2f50f8b06f7f4ab691c17043155c0735020f87e1
Deivid Mladenov <dmladenov@zingasoft.com>
2020-06-25 23:32:13 +0300

    Version 1.3

---
+++ IntroToProgrammingCSharpTasks/AgeAfter10Years/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            Console.WriteLine("How old are you?");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You will be {age+10} years old after 10 years!");
+        }
+    }
+}
---
+++ IntroToProgrammingCSharpTasks/TheUniqueNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TheUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ageAfter10Years = 0;
+            Console.WriteLine("How old are you?");
+            ageAfter10Years = int.Parse(Console.ReadLine()) + 10;
+            Console.WriteLine("Which is your birth year?");
+            int birthYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Which is your birth month?");
+            int birthMonth = int.Parse(Console.ReadLine());
+            double uniqueNumber = (ageAfter10Years * birthYear) / Math.PI * birthMonth;
+            Console.WriteLine($"Your unique number is {Math.Round(uniqueNumber, 4)}!");
+        }
+    }
+}
