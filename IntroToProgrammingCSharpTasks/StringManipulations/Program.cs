30d5bca3df2fe7810552f4ab2dd5bd84b42b8a6b
Deivid Mladenov <dmladenov@zingasoft.com>
2020-06-29 00:09:37 +0300

    Version 1.5

---
+++ IntroToProgrammingCSharpTasks/StringManipulations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringManipulations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, I am Deivid Mladenov and I appreciate your attention!";
+            Console.WriteLine($"Starting text ---> {text}");
+            Console.WriteLine($"To lower ---> {text.ToLower()}");
+            Console.WriteLine($"To upper ---> {text.ToUpper()}");
+            Console.WriteLine($"IndexOf Deivid ---> {text.IndexOf("Deivid")}");
+            Console.WriteLine($"Replace attention with work ---> {text.Replace("attention", "work")}");
+        }
+    }
+}
